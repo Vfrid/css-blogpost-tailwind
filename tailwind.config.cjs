4558d378d16305e3a2616cf3c7df4291a94e8a5f
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-09-14 10:26:07 -0400

    fixed tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
@@ -13,10 +15,24 @@ module.exports = {
       height: {
         18: "4.5rem",
       },
-      colors: {
-        "transparent-white": "rgba(255, 255, 255, 0.87)",
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'green': {
+        100: '#E4F79E',
+        200: '#CDE295',
+        300: '#C1D57B',
+        400: '#22d3ee',
+        500: '#82D03B',
+        600: '#E4F79E',
+        700: '#4FA952',
+        800: '#4E7E23 ',
+        900: '#2C4615',
       },
+      // ...
     },
   },
-  plugins: [],
-};
+},
+plugins: [],
+}
